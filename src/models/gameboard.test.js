2b0e34f102a994862f2c2b8103d25968c1495cad
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-01-08 13:54:58 -0500

    [red] add test for gameboard to place ship at valid horizontal coordinates

--- src/models/gameboard.test.js
+++ src/models/gameboard.test.js
@@ -3,7 +3,7 @@ import Gameboard from "./gameboard";
 import Ship from "./ship";
 
 it('initializes 10x10 gameboard', () => {
-    const array = [...Array(10)].map(x=>Array(10).fill(null))    
+    const array = [...Array(10)].map(x => Array(10).fill(null));    
     const gameboard = new Gameboard();
     expect(gameboard.board).toStrictEqual(array);
 });
@@ -30,4 +30,17 @@ it('returns false when placing ship at invalid vertical coordinates', () => {
     const gameboard = new Gameboard();
     const ship = new Ship(3);
     expect(gameboard.placeShip(ship, 8, 0, true)).toBe(false);
+});
+
+it('places a ship at valid horizontal coordinates', () => {
+    const ship = new Ship(3);
+
+    const board = [...Array(10)].map(x => Array(10).fill(null));
+    board[0][0] = ship;
+    board[0][1] = ship;
+    board[0][2] = ship;
+
+    const gameboard = new Gameboard();
+    gameboard.placeShip(ship, 0, 0, false);
+    expect(gameboard.board).toStrictEqual(board);
 });
