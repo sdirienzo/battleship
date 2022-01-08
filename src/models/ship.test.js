9fdaa5d20aa5010854e3023fe79c6059a9800799
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-01-08 11:18:25 -0500

    [green] add test for ship stores coordinates of hit

--- src/models/ship.test.js
+++ src/models/ship.test.js
@@ -13,4 +13,16 @@ it('gets hit', () => {
         y: 0
     });
     expect(ship.hits.length).toBe(1);
-})
+});
+
+it('stores coordinates of hit', () => {
+    const ship = new Ship(3);
+    ship.hit({
+        x: 0,
+        y: 0
+    });
+    expect(ship.hits[0]).toStrictEqual({
+        x: 0,
+        y: 0
+    });
+});
