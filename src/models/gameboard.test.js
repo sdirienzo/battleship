3d086ee1703e88db23b12d17196a484537841269
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-01-08 12:35:02 -0500

    [refactor] remove test for gameboard to initialize with 10 rows

--- src/models/gameboard.test.js
+++ src/models/gameboard.test.js
@@ -1,11 +1,6 @@
 import { expect, it } from "@jest/globals";
 import Gameboard from "./gameboard";
 
-it('initializes gameboard with 10 rows', () => {
-    const gameboard = new Gameboard();
-    expect(gameboard.board.length).toBe(10);
-});
-
 it('initializes 10x10 gameboard', () => {
     const array = [...Array(10)].map(x=>Array(10).fill(null))    
     const gameboard = new Gameboard();
