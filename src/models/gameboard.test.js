9dc94c53e6d91ae32c655b78b3243d0bcfe9eb2b
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-01-08 12:32:07 -0500

    [red] add test for gameboard to initialize 10x10 board

--- src/models/gameboard.test.js
+++ src/models/gameboard.test.js
@@ -5,3 +5,10 @@ it('initializes gameboard with 10 rows', () => {
     const gameboard = new Gameboard();
     expect(gameboard.board.length).toBe(10);
 });
+
+it('initializes 10x10 gameboard', () => {
+    const array = [...Array(10)].map(x=>Array(10).fill(null))    
+    const gameboard = new Gameboard();
+    console.log(gameboard.board);
+    expect(gameboard.board).toStrictEqual(array);
+});
