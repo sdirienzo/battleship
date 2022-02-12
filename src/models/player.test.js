af4cc8947f4599b5cc970f580d8de4e293fe7d4e
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-02-12 14:06:34 -0500

    [red] add test for player to not take invalid turn with already attacked occupied coordinates

--- src/models/player.test.js
+++ src/models/player.test.js
@@ -50,3 +50,24 @@ it('allows player to take valid turn with unoccupied coordinate', () => {
 
     expect(humanPlayer.takeTurn(gameboard, 0, 2)).toBe(true);
 });
+
+it('doesn\'t allow player to take invalid turn with already attacked occupied coordinate', () => {
+    const humanPlayer = new Player(true);
+    const gameboard = new Gameboard();
+
+    const carrier = new Ship(5);
+    const battleship = new Ship(4);
+    const destroyer = new Ship(3);
+    const submarine = new Ship(3);
+    const patrolBoat = new Ship(2);
+
+    gameboard.placeShip(carrier, 9, 4, false);
+    gameboard.placeShip(battleship, 2, 0, true);
+    gameboard.placeShip(destroyer, 2, 5, false);
+    gameboard.placeShip(submarine, 5, 9, true);
+    gameboard.placeShip(patrolBoat, 0, 0, false);
+
+    humanPlayer.takeTurn(gameboard, 0, 2);
+
+    expect(humanPlayer.takeTurn(gameboard, 0, 2)).toBe(false);
+});
