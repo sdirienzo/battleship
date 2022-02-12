f26f62dafafa3c2d0b22fd85b1918971737ffd7e
Stephen Di Rienzo <stephen.dirienzo@gmail.com>
2022-02-12 13:39:04 -0500

    [green] add test for player to identify computer player

--- src/models/player.test.js
+++ src/models/player.test.js
@@ -5,4 +5,9 @@ import Gameboard from "./gameboard";
 it('identifies human player', () => {
     const humanPlayer = new Player(true);
     expect(humanPlayer.isHuman).toBe(true);
-})
+});
+
+it('identifies computer player', () => {
+    const humanPlayer = new Player(false);
+    expect(humanPlayer.isHuman).toBe(false);
+});
